31544f7c8f3b00a4c9ffe02e238657f64225cdd0
sinsuirakv0 <239111721+sinsuirakv0@users.noreply.github.com>
2026-01-12 03:30:13 +0900

    Add files via upload

---
+++ js/ogp-loader.js
@@ -0,0 +1,14 @@
+const ogpData = `
+<meta property="og:title" content="KBC HOME">
+<meta property="og:description" content="KBC HOME">
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://kbc-rakv0.vercel.app">
+<meta property="og:image" content="https://kbc-rakv0.vercel.app/logo.png">
+<meta property="og:site_name" content="KBC HOME">
+<meta name="twitter:card" content="summary">
+<meta name="twitter:title" content="KBC HOME">
+<meta name="twitter:description" content="KBC HOME">
+<meta name="twitter:image" content="https://kbc-rakv0.vercel.app/logo.png">
+<link rel="icon" href="logo.png" type="image/png">
+`;
+document.head.insertAdjacentHTML('afterbegin', ogpData);
