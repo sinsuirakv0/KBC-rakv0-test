1a0bebce16b13af82b0e86f9ef09b30b7ea2d584
sinsuirakv0 <239111721+sinsuirakv0@users.noreply.github.com>
2026-01-13 18:09:37 +0900

    Update ogp-loader.js

--- js/ogp-loader.js
+++ js/ogp-loader.js
@@ -1,13 +1,13 @@
 const ogpData = `
-<meta property="og:title" content="KBC HOME">
-<meta property="og:description" content="KBC HOME">
+<meta property="og:title" content="KBC HOME1">
+<meta property="og:description" content="KBC HOME2">
 <meta property="og:type" content="website">
 <meta property="og:url" content="https://kbc-rakv0.vercel.app">
 <meta property="og:image" content="https://kbc-rakv0.vercel.app/logo.png">
-<meta property="og:site_name" content="KBC HOME">
+<meta property="og:site_name" content="KBC HOME3">
 <meta name="twitter:card" content="summary">
-<meta name="twitter:title" content="KBC HOME">
-<meta name="twitter:description" content="KBC HOME">
+<meta name="twitter:title" content="KBC HOME4">
+<meta name="twitter:description" content="KBC HOME5">
 <meta name="twitter:image" content="https://kbc-rakv0.vercel.app/logo.png">
 <link rel="icon" href="logo.png" type="image/png">
 `;
