dcc6eb1938b2ca97a139b05c3d18f421033f41a7
sinsuirakv0 <239111721+sinsuirakv0@users.noreply.github.com>
2026-01-30 18:10:34 +0900

    Update secret.js

--- js/secret.js
+++ js/secret.js
@@ -175,7 +175,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 showMsg("認証成功！移動します…", true);
                 setTimeout(() => {
                     closeOverlay();
-                    window.location.href = "secret-index.html";
+                    window.location.href = "secret/index.html";
                 }, 600);
             } else {
                 showMsg("コードが違いますよ笑。");
