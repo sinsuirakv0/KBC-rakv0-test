14fc75f56cdef228c4cf80fb0f620b011c8d1a41
sinsuirakv0 <239111721+sinsuirakv0@users.noreply.github.com>
2026-01-31 00:46:36 +0900

    Update getData.js

--- api/getData.js
+++ api/getData.js
@@ -3,10 +3,15 @@ import { getJWT } from "../js/jwt.js";
 export default async function handler(req, res) {
   try {
     const type = req.query.type || "gatya";
+    const urlOnly = req.query.urlOnly === "1";
 
     const jwt = await getJWT();
-
     const url = `https://nyanko-events.ponosgames.com/battlecats_production/${type}.tsv?jwt=${jwt}`;
+
+    // URL だけ返すモード
+    if (urlOnly) {
+      return res.status(200).send(url);
+    }
 
     const response = await fetch(url);
     const text = await response.text();
